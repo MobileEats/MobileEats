27f64846cc793a6a70050d757b81bd1dc0c2340a
Amdrew Walsh <andrew.jordan.walsh@gmail.com>
2021-06-22 09:41:11 -0500

    created security config

--- src/main/java/com/capstone/mobileeats/repositories/VendorRepository.java
+++ src/main/java/com/capstone/mobileeats/repositories/VendorRepository.java
@@ -1,5 +1,6 @@
 package com.capstone.mobileeats.repositories;
 
+import com.capstone.mobileeats.models.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VendorRepository extends JpaRepository {}
+public interface VendorRepository extends JpaRepository<Vendor, Long> {}
