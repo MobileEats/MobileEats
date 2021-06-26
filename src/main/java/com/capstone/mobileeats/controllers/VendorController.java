fe11135a2ae8016cb067851ff770bf5fd759f6ec
Roland Valdez <valdez.roland.23@gmail.com>
2021-06-26 02:38:50 -0500

    fixed display of mapbox and likned index to individual pages

--- src/main/java/com/capstone/mobileeats/controllers/VendorController.java
+++ src/main/java/com/capstone/mobileeats/controllers/VendorController.java
@@ -6,6 +6,7 @@ import com.capstone.mobileeats.models.Vendor;
 import com.capstone.mobileeats.models.VendorCategory;
 import com.capstone.mobileeats.repositories.VendorRepository;
 import com.capstone.mobileeats.services.EmailService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -70,9 +71,14 @@ public class VendorController {
 
     @GetMapping("/vendors/profile/{id}")
     public String show(@PathVariable long id, Model model){
+
         Vendor vendor = vendorDao.getById(id);
+
         model.addAttribute("vendorId", id);
         model.addAttribute("vendor", vendor);
+//        model.addAttribute("location", vendor.getLocation());
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        result.addObject("location", objectMapper.writeValueAsString(location));
         return "vendorProfile";
 
     }
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,13 +1,10 @@
 "use strict";
-$(document).ready(function () {
+// $(document).ready(function () {
 
 
 // ************* DISPLAY MAP ON LOAD - Default to San Antonio ******************
-//     var coord = [32.3863, -94.8758]; // lat[0] long[1] standard format
     var coord = [29.4241, -98.4936]; // lat[0] long[1] standard format
-    var lngLat = coord;
 
-    var city = "San Antonio, TX"//SA coordinates in Mapbox format
     mapboxgl.accessToken = mapboxToken;
     var mapOptions = {
         container: 'map',
@@ -17,17 +14,25 @@ $(document).ready(function () {
     }
     var map = new mapboxgl.Map(mapOptions);
     var marker = new mapboxgl.Marker({color: "red", draggable: true})
-        .setLngLat([coord[1], coord[0]])
-        .addTo(map);
-    // getWeather(coord);
-    marker.on('dragend', onDragEnd);
 
-//
+
+//Find lat long from Address
+$(document).ready(function () {
+        geocode($('.address').html(), mapboxToken).then(function (results) {
+            mapOptions.center = results;
+            marker.remove();
+            map.flyTo({center: results, zoom: 15, duration: 9000});
+            marker = new mapboxgl.Marker({color: "red", draggable: true})
+                .setLngLat(results)
+                .addTo(map);
+            marker.on('dragend', onDragEnd);
+        });
+    });
+
+
 //     // ************* GET GEOLOCATION ******************
-    $(".geolocation").click(function () {
-
+    $("#locate").click(function () {
         geoLocation();
-
     });
     function geoLocation(){
         var options = {
@@ -38,9 +43,9 @@ $(document).ready(function () {
 
         function success(pos) {
             var crd = pos.coords;
-            coord = [crd.latitude,crd.longitude];
+            var coord = [crd.latitude,crd.longitude];
             map = new mapboxgl.Map(mapOptions);
-            lngLat = [coord[1], coord[0]];
+            var lngLat = [coord[1], coord[0]];
             map.flyTo({center: lngLat, zoom: 9.7, duration: 5000})
             marker = new mapboxgl.Marker({color: "red", draggable: true})
                 .setLngLat(lngLat)
@@ -48,7 +53,7 @@ $(document).ready(function () {
             console.log("geo coord", coord);
             console.log("geo lngLat", lngLat);
             marker.on('dragend', onDragEnd);
-            getWeather(coord);
+
         }
         function error(err) {
             console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -56,66 +61,17 @@ $(document).ready(function () {
         navigator.geolocation.getCurrentPosition(success, error, options);
     }
 
-// // // ************* GET LOCATION FROM SEARCH BOX******************
-//
-//     $("#weatherBtn").click(search);
-//     function search(e) {
-//         e.preventDefault();
-//         geocode($("#weatherLocation").val(), mapboxToken).then(function (results) {
-//             mapOptions.center = results;
-//             coord = [results[1], results[0]];// in open weather format for getWeather()
-//             marker.remove();
-//             map.flyTo({center: results, zoom: 9.7, duration: 5000});
-//             marker = new mapboxgl.Marker({color: "red", draggable: true})
-//                 .setLngLat(results)
-//                 .addTo(map);
-//             marker.on('dragend', onDragEnd);
-//             getWeather(coord);
-//         });
-//     }
-//
+
 // ************* GET LOCATION FROM MARKER DRAG ******************
     function onDragEnd() {
-        coord = marker.getLngLat();
+        var coord = marker.getLngLat();
         console.log("drag coord", coord);
-        lngLat = coord;
+        var lngLat = coord;
         console.log("drag lngLat", lngLat);
 
         map.flyTo({center: lngLat, zoom: 9.7, duration: 1000});
         coord = [coord.lat, coord.lng];
-        getWeather(coord);
+
     }
 
-// // ************* MAP SETTINGS ******************
-//
-//     $("#settingBtn").click(function(){
-//         coord = marker.getLngLat();
-//         mapOptions.center = coord;
-//         if($(".mapView").val() === "satellite"){
-//             mapOptions.style = 'mapbox://styles/mapbox/satellite-streets-v11';
-//             map = new mapboxgl.Map(mapOptions);
-//             marker = new mapboxgl.Marker({color: "red", draggable: true})
-//                 .setLngLat(mapOptions.center)
-//                 .addTo(map);
-//         }else{
-//             mapOptions.style = 'mapbox://styles/mapbox/streets-v11';
-//             map = new mapboxgl.Map(mapOptions);
-//             marker = new mapboxgl.Marker({color: "red", draggable: true})
-//                 .setLngLat(mapOptions.center)
-//                 .addTo(map);
-//         }
-//         if($("#darkMode").prop("checked") === true){
-//
-//         }else{
-//
-//         }
-//         if($("#alerts").prop("checked") !== true){
-//             $("#mapOverlay").addClass("hidden");
-//         }else{
-//             $("#mapOverlay").removeClass("hidden");
-//         }
-//         marker.on('dragend', onDragEnd);
-//     });
-
-
-});
+// });
