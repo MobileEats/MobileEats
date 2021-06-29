fa0c18e37c0930d4a6874571526070824be347fb
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-29 16:00:51 -0500

    testing follow button

--- src/main/resources/static/js/vendor-profile.js
+++ src/main/resources/static/js/vendor-profile.js
@@ -1,18 +1,17 @@
-$( "#open" ).on( "click", function() {
-   openLocation();
+$("#open").on("click", function () {
+    openLocation();
 
 });
 
 
-
-function openLocation(){
+function openLocation() {
     let id = $('.vendorId').val();
     let location = $('.address').html();
     console.log(location);
     let urlTest = "/vendors/profile/" + id;
-    if( $( "input:checked" ).val() == "open"){
+    if ($("input:checked").val() == "open") {
         let data = {
-            "open":true,
+            "open": true,
             "location": location
         }
         $.ajax({
@@ -21,17 +20,16 @@ function openLocation(){
             data: JSON.stringify(data),
             contentType: "application/json",
             dataType: "json",
-            success: function(results) {
+            success: function (results) {
                 console.log(results);
             },
             // error: function (jqXHR) {
             //     $(document.body).text('Error: ' + jqXHR.status);
             // }
         });
-    }
-    else{
+    } else {
         let data = {
-            "open":false,
+            "open": false,
             "location": location
         }
         $.ajax({
@@ -40,7 +38,7 @@ function openLocation(){
             data: JSON.stringify(data),
             contentType: "application/json",
             dataType: "json",
-            success: function(results) {
+            success: function (results) {
                 console.log(results);
             },
             // error: function (jqXHR) {
@@ -49,3 +47,29 @@ function openLocation(){
         });
     }
 }
+
+<!--ADDING FOLLOW BUTTON FUNCTIONALITY-->
+// function followButton() {
+//     if ($("#follow").html() === "+ Follow") {
+//         $("#follow").html("&#10003; Following")
+//     } else {
+//         if (confirm("Unfollow " + $("#follow").attr("vendor") + "?")) {
+//             $("#follow").html("+ Follow");
+//         }
+//     }
+// }
+
+// function followButton() {
+    if ($("#follow").attr("value") === "true") {
+        $("#follow").html("&#10003; Following")
+    } else {
+        if ($("#follow").attr("value") === "false") {
+            $("#follow").html("+ Follow");
+        }
+    }
+// }
+
+// $("#follow").on("click", function () {
+//     followButton();
+// });
+
