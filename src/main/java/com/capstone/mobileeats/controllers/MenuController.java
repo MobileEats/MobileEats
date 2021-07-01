9b9b8d9d1b4ee05e39afe2a864344a18a95d5fcd
Amdrew Walsh <andrew.jordan.walsh@gmail.com>
2021-07-01 15:07:47 -0500

    functional delete button for menu items

--- src/main/java/com/capstone/mobileeats/controllers/MenuController.java
+++ src/main/java/com/capstone/mobileeats/controllers/MenuController.java
@@ -1,5 +1,6 @@
 package com.capstone.mobileeats.controllers;
 
+import com.capstone.mobileeats.models.Menu;
 import com.capstone.mobileeats.models.MenuItem;
 import com.capstone.mobileeats.models.Vendor;
 import com.capstone.mobileeats.repositories.MenuItemRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -32,5 +34,13 @@ public class MenuController {
         model.addAttribute("vendor", vendor);
         model.addAttribute("items", items);
         return "showMenu";
+    }
+
+    @GetMapping("/vendors/{vendorId}/menu/{menuItemId}/delete")
+    public String deleteMenuItem(@PathVariable Long vendorId, @PathVariable Long menuItemId, Model model){
+        MenuItem item = menuItems.getById(menuItemId);
+        menuItems.delete(item);
+        System.out.println("HELLO!");
+        return "redirect:/vendors/" + vendorId + "/menu";
     }
 }
