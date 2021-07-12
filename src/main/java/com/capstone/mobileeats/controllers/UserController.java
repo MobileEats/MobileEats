9834040f03aa6b7dd88b3bca2d32279e42952c05
Jake Chadwell <jake.chadwell01@gmail.com>
2021-07-12 09:52:14 -0500

    completed edit password functionality

--- src/main/java/com/capstone/mobileeats/controllers/UserController.java
+++ src/main/java/com/capstone/mobileeats/controllers/UserController.java
@@ -137,17 +137,19 @@ public class UserController {
 
     //edit password
     @GetMapping("/users/{id}/editPassword")
-    public String showEditPassword(@PathVariable long id, @ModelAttribute Model model){
-        model.addAttribute("user", users.getById(id));
+    public String showEditPassword(@PathVariable long id){
         return "editPassword";
     }
 
     @PostMapping("/users/{id}/editPassword")
-    public String editPassword(@RequestParam String oldPassword, @RequestParam String newPassword, @ModelAttribute User user){
+    public String editPassword(@PathVariable long id, @RequestParam String oldPassword, @RequestParam String newPassword){
+        User user = users.getById(id);
         String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
         if(Objects.isNull(user)){
             return "redirect:/editPassword";
         }
+        System.out.println("user.getPassword() = " + user.getPassword());
+        System.out.println("oldPassword = " + oldPassword);
         if(BCrypt.checkpw(oldPassword, user.getPassword())){
             user.setPassword(hashedPassword);
             users.save(user);
